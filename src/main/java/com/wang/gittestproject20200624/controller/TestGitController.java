8127f0cf11a90b69a96c842578b13b4fb00221e0
wanglichao <wanglichaoya@163.com>
2020-06-24 18:54:19 +0800

    git-dev 测试版本提交

--- src/main/java/com/wang/gittestproject20200624/controller/TestGitController.java
+++ src/main/java/com/wang/gittestproject20200624/controller/TestGitController.java
@@ -14,8 +14,8 @@ public class TestGitController {
 
     @GetMapping("/test")
     public String test() {
-        System.out.println("这个同学A开发的版本");
-        System.out.println("合并到主分支然后提交");
+        System.out.println("test1");
+
         return "hello world";
     }
 }
